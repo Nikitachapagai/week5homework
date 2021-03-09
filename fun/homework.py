290dfa5eb776d0d79b78ccb9aa5099bee7fd31cb
VCU- Chapagain <78453389+Nikitachapagai@users.noreply.github.com>
2021-03-08 21:45:43 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,10 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    return max(incoming_list)
+    if len(incoming_list) == 0:
+        return 0
+    else:
+        return max(incoming_list)
 
 
 def find_least_number(incoming_list):
@@ -14,7 +17,10 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    return min(incoming_list)
+    if len(incoming_list) == 0:
+        return 0
+    else:
+        return min(incoming_list)
 
 
 def add_list_numbers(incoming_list):
@@ -22,7 +28,10 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-  return sum(incoming_list)  
+    if len(incoming_list) == 0:
+        return 0
+    else:
+        return sum(incoming_list)
 
 
 def longest_value_key(incoming_dict):
@@ -30,4 +39,8 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    pass
+    for item, value in incoming_dict.items():
+        
+
+
+print(longest_value_key({'name': 'Jack', 'age': "27", 'address': 'Downtown'}))
